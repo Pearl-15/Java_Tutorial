5b7e9d2aa29dc1eea84828917629da5c250466bd
Pearl Khin <pearlkhin@Pearls-Mac-mini.local>
2024-12-13 17:04:50 +0800

    feat: create HelloWorldConfiguration

--- src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
@@ -1,21 +1,20 @@
 package com.in28minutes.learn_spring_framework;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.MarioGame;
 import com.in28minutes.learn_spring_framework.game.PacManGame;
 import com.in28minutes.learn_spring_framework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		var game = new MarioGame();
 		
-//		var game = new SuperContraGame();
+		// 1. launch a Spring Context
 		
-		var game = new PacManGame();
-		var gameRunner = new GameRunner(game);
-		gameRunner.run();
+		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+		// 2. Configure the things that we want Spring to manage -@Configuraiton
 
 	}
 
